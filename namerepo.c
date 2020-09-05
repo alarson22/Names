eba5185a372768493f6f36c969b233ae125a9d41
alarson22 <69922454+alarson22@users.noreply.github.com>
2020-09-04 22:05:02 -0500

    Initial file upload

---
+++ namerepo.c
@@ -0,0 +1,13 @@
+/**
+ * Author: Andrew Larson (CSE: alarson)
+ * 9/4/2020
+ * Assignment 1 names 
+ */
+
+ #include<stdlib.h>
+ #include<stdio.h>
+
+ void main(int argc, char **argv){
+	printf("Andrew Larson\n");		
+
+}
